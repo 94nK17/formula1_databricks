426c47460adc159576d6a7d012eff67584a64d37
ankitpanwar32@gmail.com <ankitpanwar32@gmail.com>
2025-12-05 16:36:48 +0000

    first

---
+++ Formula1_git/ingsestion/ingest_all_files.py
@@ -0,0 +1,74 @@
+# Databricks notebook source
+v_result = dbutils.notebook.run("ingest_circuits", 0, {"p_data_source": "Ergast API", "p_file_date": "2021-04-18"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("ingest_races", 0, {"p_data_source": "Ergast API", "p_file_date": "2021-04-18"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("ingest_constructors", 0, {"p_data_source": "Ergast API", "p_file_date": "2021-04-18"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("ingest_drivers", 0, {"p_data_source": "Ergast API", "p_file_date": "2021-04-18"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("ingest_results", 0, {"p_data_source": "Ergast API", "p_file_date": "2021-04-18"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("ingest_pit_stops", 0, {"p_data_source": "Ergast API", "p_file_date": "2021-04-18"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("ingest_lap_times", 0, {"p_data_source": "Ergast API", "p_file_date": "2021-04-18"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+v_result = dbutils.notebook.run("ingest_qualifying", 0, {"p_data_source": "Ergast API", "p_file_date": "2021-04-18"})
+
+# COMMAND ----------
+
+v_result
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select race_id, count(*)
+# MAGIC   from f1_processed.results
+# MAGIC  group by race_id
+# MAGIC  order by race_id desc;
+
+# COMMAND ----------
+
+
